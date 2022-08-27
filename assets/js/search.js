e92f783955eefe7266f675086044eacfa8cb5f6e
saxenism <saxenism@users.noreply.github.com>
2022-08-27 22:02:55 +0000

    deploy: 2fc30e6d2a4bdc28ab57e2e84209294b5ab9307e

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/expressions/assets/js/search-data.json', true);
+    request.open('GET', '/saxenism-expressions/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
